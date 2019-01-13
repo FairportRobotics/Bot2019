21d9ac4b17c4c90993c5b3dee478f69137eb0490
Kevin <h1d3m3@yahoo.com>
2019-01-13 11:39:30 -0500

    Updated class to support new timed robot class. Updated imports. Ignore idea iml files.

--- src/main/java/team578/robot/Robot.java
+++ src/main/java/team578/robot/Robot.java
@@ -1,14 +1,66 @@
 package team578.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.TimedRobot;
 
-public class Robot {
+public class Robot extends TimedRobot {
 	// variables for talons
-	private WPI_TalonSRX rightMaster;
-	private WPI_TalonSRX rightSlave;
-	private WPI_TalonSRX leftMaster;
-	private WPI_TalonSRX leftSlave;
+	private TalonSRX rightMaster;
+	private TalonSRX rightSlave;
+	private TalonSRX leftMaster;
+	private TalonSRX leftSlave;
 	
 	private AHRS navx;
+
+	@Override
+	public void robotInit() {
+		super.robotInit();
+	}
+
+	@Override
+	public void disabledInit() {
+		super.disabledInit();
+	}
+
+	@Override
+	public void autonomousInit() {
+		super.autonomousInit();
+	}
+
+	@Override
+	public void teleopInit() {
+		super.teleopInit();
+	}
+
+	@Override
+	public void testInit() {
+		super.testInit();
+	}
+
+	@Override
+	public void robotPeriodic() {
+		super.robotPeriodic();
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		super.disabledPeriodic();
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+		super.autonomousPeriodic();
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		super.teleopPeriodic();
+	}
+
+	@Override
+	public void testPeriodic() {
+		super.testPeriodic();
+	}
 }
