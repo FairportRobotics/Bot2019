d9a1be313287f5ddbd8a2b38b06ea2921a0c67da
kdog10 <craftastickarla@gmail.com>
2019-01-26 12:47:34 -0500

    changed main talon value to 55

--- src/main/java/frc/team578/robot/RobotMap.java
+++ src/main/java/frc/team578/robot/RobotMap.java
@@ -3,7 +3,7 @@ package frc.team578.robot;
 public class RobotMap {
 
     // motor controller IDs
-    public static final int MAIN_TALON_ID = 7;
+    public static final int MAIN_TALON_ID = 55;
 
     // swerve controller joystick IDs
     public static final int CONTROL_GAMEPAD_ID = 0;
