037db13ec80da155fed6f8dfcd8a7ae83e237791
Trifex <techwizguy7@gmail.com>
2019-01-29 19:30:42 -0500

    Added joystick code to toggle the piston

--- src/main/java/frc/team578/robot/Robot.java
+++ src/main/java/frc/team578/robot/Robot.java
@@ -1,49 +1,50 @@
 package frc.team578.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-
-import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.lang.*;
 
 public class Robot extends TimedRobot {
+
+	// Joystick pointer
 	private static Joystick joystick;
 
-	DoubleSolenoid testDouble;
+	/* DoubleSolenoid.Value list
+		kOff is off (duh)
+		kForward is extended
+		kReverse is retracted
+	*/
+	private DoubleSolenoid testDouble; // piston pointer
+	private boolean pistonExtended; // state of the piston, will be found on robot startup (!)
 
 	public void robotInit() {
 		System.out.println("Turned robot on");
-		testDouble = new DoubleSolenoid(0,1);
 
+		// assign pointers to their values
+		testDouble = new DoubleSolenoid(RobotMap.DOUBLE_SOLENOID_ID, RobotMap.DS_FORWARD_CHANNEL_ID, RobotMap.DS_REVERSE_CHANNEL_ID);
 		joystick = new Joystick(RobotMap.CONTROL_GAMEPAD_ID);
 	}
 
 	public void disabledInit() {
 		System.out.println("Disabled robot :sad face:");
+		pistonExtended = testDouble.get().equals(DoubleSolenoid.Value.kForward) ? true : false; // gets disabled on startup, no worries
 	}
-
 
 	public void teleopInit() {
 		System.out.println("Enabled in teleop mode");
-
-//		testDouble.set(DoubleSolenoid.Value.kOff);
-//		testDouble.set(DoubleSolenoid.Value.kForward);
-		testDouble.set(DoubleSolenoid.Value.kReverse);
 	}
 
-	public void robotPeriodic() {
-
-	}
-
-	public void disabledPeriodic() {
-
-	}
+	// long currentTime = System.currentTimeMillis();
 
 	public void teleopPeriodic() {
-		double stick = joystick.getRawAxis(RobotMap.JOYSTICK_X_AXIS_ID);
+		boolean button1Pressed = joystick.getRawButton(RobotMap.BUTTON_1_ID);
+		if(button1Pressed) {
+			if(pistonExtended) {
+				testDouble.set(DoubleSolenoid.Value.kReverse);
+				pistonExtended = false;
+			} else if(!pistonExtended) {
+				testDouble.set(DoubleSolenoid.Value.kForward);
+				pistonExtended = true;
+			}
+		}
 	}
 }
