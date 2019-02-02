fe4db7b1da8a3df8f536342de13b7a5748784c87
Kevin <h1d3m3@yahoo.com>
2019-02-02 16:31:31 -0500

    Clean up code for base test board operations.

--- src/main/java/frc/team578/robot/Robot.java
+++ src/main/java/frc/team578/robot/Robot.java
@@ -8,43 +8,19 @@ public class Robot extends TimedRobot {
 	// Joystick pointer
 	private static Joystick joystick;
 
-	/* DoubleSolenoid.Value list
-		kOff is off (duh)
-		kForward is extended
-		kReverse is retracted
-	*/
-	private DoubleSolenoid testDouble; // piston pointer
-	private boolean pistonExtended; // state of the piston, will be found on robot startup (!)
-
 	public void robotInit() {
 		System.out.println("Turned robot on");
-
-		// assign pointers to their values
-		testDouble = new DoubleSolenoid(RobotMap.DOUBLE_SOLENOID_ID, RobotMap.DS_FORWARD_CHANNEL_ID, RobotMap.DS_REVERSE_CHANNEL_ID);
-		joystick = new Joystick(RobotMap.CONTROL_GAMEPAD_ID);
 	}
 
 	public void disabledInit() {
 		System.out.println("Disabled robot :sad face:");
-		pistonExtended = testDouble.get().equals(DoubleSolenoid.Value.kForward) ? true : false; // gets disabled on startup, no worries
 	}
 
 	public void teleopInit() {
 		System.out.println("Enabled in teleop mode");
 	}
 
-	// long currentTime = System.currentTimeMillis();
-
 	public void teleopPeriodic() {
-		boolean button1Pressed = joystick.getRawButton(RobotMap.BUTTON_1_ID);
-		if(button1Pressed) {
-			if(pistonExtended) {
-				testDouble.set(DoubleSolenoid.Value.kReverse);
-				pistonExtended = false;
-			} else if(!pistonExtended) {
-				testDouble.set(DoubleSolenoid.Value.kForward);
-				pistonExtended = true;
-			}
-		}
+		
 	}
 }
--- src/main/java/frc/team578/robot/RobotMap.java
+++ src/main/java/frc/team578/robot/RobotMap.java
@@ -20,11 +20,4 @@ public class RobotMap {
 
     public static final int BUTTON_4_ID = 4; // button 4 id
 
-//  === DOUBLE SOLENOID STUFF ===
-    public static final int DOUBLE_SOLENOID_ID = 4; // double solenoid id
-
-    public static final int DS_FORWARD_CHANNEL_ID = 0; // double solenoid forward channel id
-
-    public static final int DS_REVERSE_CHANNEL_ID = 1; // double solenoid reverse channel id
-
 }
