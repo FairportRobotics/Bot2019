60d2863b165f9722377b3e241963b381198e2ab5
Kevin <h1d3m3@yahoo.com>
2019-02-17 14:22:44 -0500

    Updated true north

--- src/main/java/frc/team578/robot/subsystems/swerve/SwerveConstants.java
+++ src/main/java/frc/team578/robot/subsystems/swerve/SwerveConstants.java
@@ -6,14 +6,10 @@ public class SwerveConstants {
     public static final double ROBOT_LENGTH = .625;
     public static final double ROBOT_WIDTH = .47;
 
-    // Analog: Pos: -485u   |   Vel: 0u/100ms   |   ADC:539   |   1.7 V
-    public static final int FRONT_LEFT_TRUE_NORTH_ENC_POS = 605;
-    // Analog: Pos: -453u   |   Vel: 0u/100ms   |   ADC:571   |   1.7 V
-    public static final int FRONT_RIGHT_TRUE_NORTH_ENC_POS = 644;
-    // Analog: Pos: 245u   |   Vel: 0u/100ms   |   ADC:245   |   0.8 V
-    public static final int BACK_LEFT_TRUE_NORTH_ENC_POS = 273;
-    // Analog: Pos: 679u   |   Vel: 2u/100ms   |   ADC:679   |   2.2 V
-    public static final int BACK_RIGHT_TRUE_NORTH_ENC_POS = 776;
+    public static final int FRONT_LEFT_TRUE_NORTH_ENC_POS = 645;
+    public static final int FRONT_RIGHT_TRUE_NORTH_ENC_POS = 160;
+    public static final int BACK_LEFT_TRUE_NORTH_ENC_POS = 879;
+    public static final int BACK_RIGHT_TRUE_NORTH_ENC_POS = 419;
 
     public static final boolean FRONT_LEFT_REVERSE_DRIVE = false;
     public static final boolean FRONT_RIGHT_REVERSE_DRIVE = true;
