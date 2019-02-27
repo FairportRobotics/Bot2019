fa5decd8a1eabff2e5a8b3cd224f525912f938bf
Kevin <h1d3m3@yahoo.com>
2019-02-26 20:41:43 -0500

    Testing shift code.

--- src/main/java/frc/team578/robot/Robot.java
+++ src/main/java/frc/team578/robot/Robot.java
@@ -1,68 +1,131 @@
 package frc.team578.robot;
 
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
 import java.lang.*;
 
 public class Robot extends TimedRobot {
 
-	// Joystick pointer
-	private static Joystick joystick;
+    // Joystick pointer
+    private static Joystick joystick;
 
-	public void robotInit() {
-		System.out.println("Robot Init");
-	}
+    Gamepad gamepad = new Gamepad(0);
+    JoystickButton rb;
+    JoystickButton lb;
+    JoystickButton rt;
+    JoystickButton lt;
+    JoystickButton buttonA;
+    JoystickButton buttonB;
+    JoystickButton buttonX;
+    JoystickButton buttonY;
+    JoystickButton back;
+    JoystickButton start;
 
-	@Override
-	public void robotPeriodic() {
-		Scheduler.getInstance().run();
-	}
+    public void robotInit() {
+        System.out.println("Robot Init");
 
-	@Override
-	public void disabledInit() {
-		System.out.println("Disabled Init");
-	}
+        gamepad = new Gamepad(0);
+        rb = gamepad.getRightShoulder();
+        lb = gamepad.getLeftShoulder();
+        rt = gamepad.getRightTriggerClick();
+        lt = gamepad.getLeftTriggerClick();
+        buttonA = gamepad.getButtonA();
+        buttonB = gamepad.getButtonB();
+        buttonX = gamepad.getButtonX();
+        buttonY = gamepad.getButtonY();
+        back = gamepad.getBackButton();
+        start = gamepad.getStartButton();
+    }
 
-	@Override
-	public void disabledPeriodic() {
-		updateDashboard();
-		Scheduler.getInstance().run();
-	}
+    @Override
+    public void robotPeriodic() {
 
-	@Override
-	public void teleopInit() {
-		System.out.println("Teleop Init");
-	}
+        back.whenPressed(new CommandPrint("BACK"));
+//        buttonY.whileHeld(new CommandPrint("Y"));
 
-	@Override
-	public void teleopPeriodic() {
-		updateDashboard();
-		Scheduler.getInstance().run();
-	}
+        Scheduler.getInstance().run();
+    }
 
-	@Override
-	public void autonomousInit() {
-		System.out.println("Auto Init");
-	}
+    @Override
+    public void disabledInit() {
+        System.out.println("Disabled Init");
+    }
 
-	@Override
-	public void autonomousPeriodic() {
-		updateDashboard();
-		Scheduler.getInstance().run();
-	}
+    @Override
+    public void disabledPeriodic() {
+        updateDashboard();
+        Scheduler.getInstance().run();
+    }
 
-	@Override
-	public void testInit() {
-		System.out.println("Test Init");
-	}
+    @Override
+    public void teleopInit() {
+        System.out.println("Teleop Init");
+    }
 
-	@Override
-	public void testPeriodic() {
-		Scheduler.getInstance().run();
-	}
+    @Override
+    public void teleopPeriodic() {
+        updateDashboard();
+        Scheduler.getInstance().run();
+    }
 
-	public void updateDashboard() {
+    @Override
+    public void autonomousInit() {
+        System.out.println("Auto Init");
+    }
 
-	}
+    @Override
+    public void autonomousPeriodic() {
+        updateDashboard();
+        Scheduler.getInstance().run();
+    }
+
+    @Override
+    public void testInit() {
+        System.out.println("Test Init");
+    }
+
+    @Override
+    public void testPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+    public void updateDashboard() {
+
+    }
+
+    class CommandPrint extends Command {
+
+        String message;
+
+        public CommandPrint(String back) {
+
+            message = back;
+        }
+
+        long last = System.currentTimeMillis();
+        protected void execute() {
+
+            if (System.currentTimeMillis() - last > 500) {
+                last = System.currentTimeMillis();
+                if (buttonY.get()) {
+                    System.err.println(System.currentTimeMillis() + " SHIFTED " + message);
+                } else {
+                    System.err.println(System.currentTimeMillis() + " " + message);
+                }
+            }
+        }
+
+        protected void end() {
+            System.err.println(message + " END");
+        }
+
+
+        @Override
+        protected boolean isFinished() {
+            return false;
+        }
+    }
 }
